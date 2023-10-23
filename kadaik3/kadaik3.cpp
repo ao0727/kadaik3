ab8e9680f369101b3d9af54dda84eada7a9429c4
ao0727 <qingduj@gmail.com>
2023-10-24 07:46:13 +0900

    プロジェクト ファイルを追加します。

---
+++ kadaik3/kadaik3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    int totalSecond;
+
+    cout << "秒数を入力 ";
+    cin >> totalSecond;
+
+    int m = totalSecond / 60;
+    int s = totalSecond % 60;
+
+    cout << setfill('0') << setw(2) << m << ":" << setw(2) << s << "秒" << endl;
+
+    
+}
